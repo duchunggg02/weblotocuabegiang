999af1d79744444e60f15f34de6f44bf247b2dae
duchunggg02 <89918323+duchunggg02@users.noreply.github.com>
2022-01-29 00:10:39 +0700

    Add files via upload

---
+++ loto.js
@@ -0,0 +1,29 @@
+var soloto = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+var dem = 0;
+function chay() {
+    dem += 1;
+    if(dem < 91)
+    {
+        tinh();
+    } 
+    else 
+    {
+        document.getElementById('kq').innerHTML = 'Đéo đứa nào kinh, dẹp mẹ đi =))'
+    }
+}
+function tinh() {
+    var kiemtra = 0;
+    var so = 0;
+    while (kiemtra == 0)
+    {
+        var x = Math.floor((Math.random()*90) + 1);
+        if (soloto[x - 1] != x)
+        {
+            soloto[x - 1] = x;
+            so = x;
+            kiemtra = 1;
+        }
+    }
+    document.getElementById(x).innerHTML = so;
+    document.getElementById('soxo').innerHTML = so;
+}
